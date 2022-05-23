6d481a2b56e7e518545db8a8512d69cfe5c8d333
Szymon Lesko <leskoszymon30@gamil.com>
2022-05-23 11:41:51 +0200

    create currency calculator

---
+++ src/App/currencies.js
@@ -0,0 +1,18 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.3593,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.6195,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.4776,
+    },
+
+];
