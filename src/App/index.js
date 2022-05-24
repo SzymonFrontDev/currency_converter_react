edaf4a0efe980b93182e0d26084bcfd1ad5a5591
Szymon Lesko <leskoszymon30@gamil.com>
2022-05-24 14:39:48 +0200

    space

--- src/App/index.js
+++ src/App/index.js
@@ -21,7 +21,7 @@ function App() {
 
     return (
         <div className="app">
-            <Clock/>
+            <Clock />
             <Form
             result={result}
             calculateResult={calculateResult}
